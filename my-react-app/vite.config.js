bf3086bacf8ebb24a0e3d0b102a15528e14ad705
ThanhQN <40609942+ThanhQN@users.noreply.github.com>
2025-08-27 12:41:48 +0000

    Add test coverage

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
     globals: true,
+    coverage: {
+      provider: 'v8', // or 'c8' if you prefer
+      reporter: ['text', 'json', 'html'], // choose your preferred formats
+    },
+
   },
 
 })
